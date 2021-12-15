c3d33e7840bba5c486c3f6f6cd6804053d1b0e8a
Biswajit-10 <https://github.com/Biswajit-10/A.git>
2021-12-15 10:44:16 +0530

    a

--- src/test/java/extentReports/Reports.java
+++ src/test/java/extentReports/Reports.java
@@ -1,5 +1,8 @@
 package extentReports;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
@@ -8,14 +11,16 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 public class Reports {
 //	Remember 3 classes 
 //	1.ExtentReports	2.ExtentSparkReporter	3.ExtentTest
-	
-	public static void main(String[] args) {
-		ExtentReports report=new ExtentReports();
+	@Test
+	public void extent() {
 		ExtentSparkReporter spark=new ExtentSparkReporter("./Extent Report/report.html");
+		ExtentReports report=new ExtentReports();
 		report.attachReporter(spark);//without this not working
 		ExtentTest logger = report.createTest("i am a test method");
 		logger.log(Status.INFO,"start");
+		logger.log(Status.INFO,"end");
 		
+		Reporter.log("testng");
 		report.flush();
 	}
 
